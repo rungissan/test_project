a8957068e6443b9ac609e29a71f5aeb0fdd6f0b3
Iurii Samoshkin <yury@samoshk.in>
2025-06-11 20:38:27 +0200

    chore: add husky for precommit tests

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   server: {
@@ -8,5 +9,10 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true
-  }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+  },
 })
